88c0cca6d89ed58ab937270f1b098a97877f053f
haulo <233421461+haulohaulo@users.noreply.github.com>
2025-11-20 12:40:37 +0000

    Add Basic Calculator Functions And HTML Layout

---
+++ javascript.js
@@ -0,0 +1,52 @@
+let operator = '+';
+let number1 = 1;
+let number2 = 2;
+
+
+
+
+function add(a, b) {
+    return a + b;
+};
+
+function subtract(a, b) {
+    return a - b;
+};
+
+
+function multiply(a, b) {
+    return a * b;
+};
+
+function divide(a, b) {
+    return a / b;
+};
+
+
+
+function operate(operator, number1, number2) {
+    if (operator = '+') {
+        return add(number1, number2);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(add(1, 2));
+
+console.log(subtract(5, 2));
+
+console.log(multiply(3, 4));
+
+console.log(divide(12, 6));
