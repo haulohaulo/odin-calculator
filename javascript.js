e04555404971dfb9bb24637eb03159ca811ed4fb
haulo <233421461+haulohaulo@users.noreply.github.com>
2025-11-28 11:50:44 +0000

    Add Code To Support Non-Single Digit Numbers

--- javascript.js
+++ javascript.js
@@ -1,40 +1,75 @@
 let operator = '+';
 let number1 = 1;
 let number2 = 2;
-let firstResult = 0;
+let result = 0;
+
+let round = 0;
 
 let performMath = [];
 
+let preRegisteredNumber = '';
+let numberBtns = document.querySelectorAll(".number");
 
-
-
+let operatorBtns = document.querySelectorAll(".operator");
 
 let displayText = document.querySelector(".text");
 let btns = document.querySelectorAll("button");
 let equalBtn = document.querySelector("#equal");
 
 
-btns.forEach((btn) => {
-    btn.addEventListener("click", (e) => {
-        performMath.push(btn.textContent);
-        displayText.textContent = performMath.join(' ');
-        console.log(performMath);
+numberBtns.forEach((numberBtn) => {
+    numberBtn.addEventListener("click", (e) => {
+        preRegisteredNumber = preRegisteredNumber + numberBtn.textContent;
         
         
         
-    })
-})
+    });
+});
+
+
+operatorBtns.forEach((operatorBtn) => {
+    operatorBtn.addEventListener("click", (e) => {
+        if (round === 0) {
+            performMath.push(preRegisteredNumber);
+            performMath.push(operatorBtn.textContent);
+            preRegisteredNumber = '';
+
+            displayText.textContent = performMath.join('');
+
+            console.log(performMath);
+        } else if (round > 0) {
+            performMath.push(operatorBtn.textContent);
+            preRegisteredNumber = '';
+
+            displayText.textContent = performMath.join('');
+
+            console.log(performMath);
+        }
+        
+    });
+});
+
+
 
 
 equalBtn.addEventListener('click', (e) => {
+    performMath.push(preRegisteredNumber);
+    
     number1 = parseInt(performMath[0]);
     operator = performMath[1];
     number2 = parseInt(performMath[2]);
+
+    console.log(performMath);    
     
-    firstResult = operate(number1, operator, number2);
-    displayText.textContent = firstResult;
-    performMath = [firstResult];
+    result = operate(number1, operator, number2);
+    displayText.textContent = result;
+    performMath = [result];
+
+    round++;
 });
+
+
+
 
 function add(a, b) {
     return a + b;
@@ -69,9 +104,6 @@ function operate(number1, operator, number2) {
         return divide(number1, number2);
     };
 };
-
-
-
 
 
 
